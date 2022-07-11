69fa2cbb88adc3dbabbd91b81ff3e79ed804146f
Adrian <52701496+futzu@users.noreply.github.com>
2022-07-11 16:16:50 +0000

    Added new_reader

--- gumd.py
+++ gumd.py
@@ -13,6 +13,7 @@ import struct
 import sys
 import urllib.request
 from functools import partial
+from new_reader import reader
 
 
 class GumD:
@@ -51,87 +52,6 @@ class GumD:
         self.sock.close()
         sys.exit()
 
-
-def reader(uri):
-    """
-    reader returns an open file handle.
-
-    stdin:              cat video.ts | gumd
-    file uri:           gumd -i /home/you/video.ts
-    http(s) uri:        gumd -i https://example.com/vid.ts
-    udp uris:           gumd -i udp://1.2.3.4:5555
-    multicast uris:     gumd -i udp://@227.1.3.10:4310
-
-    """
-    # read from stdin
-    if uri in [None,sys.stdin.buffer]:
-        return sys.stdin.buffer
-    # Multicast
-    if uri.startswith("udp://@"):
-        return open_mcast(uri)
-    # Udp
-    if uri.startswith("udp://"):
-        return open_udp(uri)
-    # Http(s)
-    if uri.startswith("http"):
-        return urllib.request.urlopen(uri)
-    # File
-    return open(uri, "rb")
-
-
-def read_stream(sock):
-    """
-    return a socket that can be read like a file.
-    """
-    return sock.makefile(mode="rb")
-
-
-def _mk_udp_sock(udp_ip, udp_port):
-    """
-    udp socket setup
-    """
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((udp_ip, udp_port))
-    return sock
-
-
-def _mk_mcast_sock(mcast_grp, mcast_port, all_grps=True):
-    """
-    multicast socket setup
-    """
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 5400000)
-    if all_grps:
-        sock.bind(("", mcast_port))
-    else:
-        sock.bind((mcast_grp, mcast_port))
-    mreq = struct.pack("4sl", socket.inet_aton(mcast_grp), socket.INADDR_ANY)
-    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-    return sock
-
-
-def open_udp(uri):
-    """
-    udp://1.2.3.4:5555
-    """
-    udp_ip, udp_port = (uri.split("udp://")[1]).split(":")
-    udp_port = int(udp_port)
-    udp_sock = _mk_udp_sock(udp_ip, udp_port)
-    return read_stream(udp_sock)
-
-
-def open_mcast(uri):
-    """
-    udp://@227.1.3.10:4310
-    """
-    mcast_grp, mcast_port = (uri.split("udp://@")[1]).split(":")
-    mcast_port = int(mcast_port)
-    mcast_sock = _mk_mcast_sock(mcast_grp, mcast_port)
-    return read_stream(mcast_sock)
-
-
 def parse_args():
     """
     parse_args parse command line args
@@ -143,9 +63,9 @@ def parse_args():
         "--input",
         default=None,
         help="""like "/home/a/vid.ts"
-                                or "udp://@235.35.3.5:3535"
-                                or "https://futzu.com/xaa.ts"
-                                """,
+                or "udp://@235.35.3.5:3535"
+                or "https://futzu.com/xaa.ts"
+               """,
     )
 
     parser.add_argument(
