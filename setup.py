b4905480adfc81bedf16199c66ab7f2aca3caee2
Adrian <52701496+futzu@users.noreply.github.com>
2023-03-15 18:32:41 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python3
 
 import setuptools
-import gumd
+import gums
 
 with open("README.md", "r") as fh:
     readme = fh.read()
 
 setuptools.setup(
-    name="gumd",
-    version=gumd.version(),
+    name="gums",
+    version=gums.version(),
     author="Adrian",
     author_email="spam@iodisco.com",
-    description="gumd, Grande Unified Multicast Daemon",
+    description="gums, Grande Unicast Multicast Sender",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url="https://github.com/futzu/gumd",
+    url="https://github.com/futzu/gums",
     install_requires=[
         "new_reader >=0.1.07",
     ],
-    py_modules=["gumd","gumc"],
-    scripts=['bin/gumd', 'bin/gumc'],
+    py_modules=["gumd","gumc","gums"],
+    scripts=['bin/gums','bin/gumd', 'bin/gumc'],
     platforms="all",
     classifiers=[
         "License :: OSI Approved :: MIT License",
